bf97b0621d6c6b97a2eeb9303169f63d31b048c6
mina1310 <mina.kavianig@gmail.com>
2026-06-02 22:27:54 +0330

    refactor: create some changes in buildHierarchy

--- src/utils/buildHierarchy.ts
+++ src/utils/buildHierarchy.ts
@@ -1,22 +1,31 @@
 export type DataList<T> = T & { children: DataList<T>[] };
 
 const buildHierarchy = <
-  T extends { id: number | string; parentId: string | number | null },
+  T extends { id: number | string; parentId: number | string | null },
 >(
   items: T[],
 ): DataList<T>[] => {
-  let map: Record<number | string, DataList<T>> = {};
-  let root = [];
-  for (let item of items) {
-    map[item.id] = { ...item, children: [] };
+  const map = new Map<T["id"], DataList<T>>();
+  const root = [];
+  for (const item of items) {
+    map.set(item.id, { ...item, children: [] });
   }
-  for (let item of items) {
-    if (item.parentId !== null) {
-      const parentNode = map[item.parentId];
-      parentNode.children.push(map[item.id]);
-    } else {
-      root.push(map[item.id]);
+  for (const item of items) {
+    const node = map.get(item.id);
+    if (!node) {
+      throw new Error(`node with ${item.id} not found in map`);
     }
+    if (item.parentId === null) {
+      root.push(node);
+      continue;
+    }
+    const parentIdNode = map.get(item.parentId);
+    if (!parentIdNode) {
+      throw new Error(
+        `parentNode ${item.parentId} not found for id:${item.id} in map`,
+      );
+    }
+    parentIdNode.children.push(node);
   }
   return root;
 };
