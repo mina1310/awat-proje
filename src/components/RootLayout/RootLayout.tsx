15b386a20a32c384b1f4833d0326623281f27a31
mina1310 <mina.kavianig@gmail.com>
2026-06-15 19:49:48 +0330

    style: add className for styling

--- src/components/RootLayout/RootLayout.tsx
+++ src/components/RootLayout/RootLayout.tsx
@@ -1,42 +1,5 @@
-// import React from "react";
-// import Header from "../Header/Header";
-// import Sidebar from "../Sidebar/Sidebar";
-// import { Outlet } from "react-router-dom";
-// import { Box, Container } from "@mui/material";
-
-// const RootLayout: React.FC = () => {
-//   return (
-//     <Box sx={{ display: "flex", direction: "rtl" }}>
-//       <Header />
-//       <Sidebar />
-//       <Box
-//         component="main"
-//         sx={{
-//           flexGrow: 1,
-//           mt: "64px",
-//           backgroundColor: "#f7f7f7",
-//           minHeight: "calc(100vh - 64px)",
-//           overflow: "auto",
-//         }}
-//       >
-//         <Container
-//           maxWidth="lg"
-//           sx={{
-//             py: 3,
-//             px: { xs: 2, sm: 3, md: 4 },
-//           }}
-//         >
-//           <Outlet />
-//         </Container>
-//       </Box>
-//     </Box>
-//   );
-// };
-// export default RootLayout;
-
 import { Outlet } from "react-router-dom";
 import { Box, Container } from "@mui/material";
-
 import styles from "./RootLayout.module.scss";
 import { Header } from "../Header";
 import { Sidebar } from "../Sidebar";
@@ -45,9 +8,7 @@ export const RootLayout = () => {
   return (
     <Box className={styles.layout}>
       <Header />
-
       <Sidebar />
-
       <Box component="main" className={styles.main}>
         <Container maxWidth="lg" className={styles.content}>
           <Outlet />
